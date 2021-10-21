9ddc0ed24b2e3fda3c2f16a9f536bd168ffba878
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-21 15:28:04 +0100

    second commit

--- src/friends.py
+++ src/friends.py
@@ -14,5 +14,31 @@ def likes_to_eat(dictionary_name, food_type):
 def add_friend(dictionary_name, friend_name):
     dictionary_name["friends"].append(friend_name)
 
+def remove_friend(dictionary_name, friend_name):
+    dictionary_name["friends"].remove(friend_name)
 
+def total_money(list_of_people):
+    total_monies = 0
+    for person in list_of_people:
+        total_monies = total_monies + person["monies"]
+    return total_monies
 
+def l_money(ler_dictionary, lee_dictionary, loan_amount):
+    # lender = lender - loan
+    ler_dictionary["monies"] = ler_dictionary["monies"] - loan_amount
+    # lendee = lendee + loan
+    lee_dictionary["monies"] = lee_dictionary["monies"] + loan_amount
+
+def all_favourite_foods(list_of_people):
+    list_of_favs = []
+    for person in list_of_people:
+        for snack in person["favourites"]["snacks"]:
+            list_of_favs.append(snack)
+    return list_of_favs
+
+def find_no_friendends(people):
+    no_friends = []
+    for person in people:
+        if len(person['friends']) == 0:
+            no_friends.append(person)
+    return no_friends
