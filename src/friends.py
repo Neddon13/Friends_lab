438bbec73a3b2aac0381121434ce6e925fd5415c
Neddon13 <91260623+Neddon13@users.noreply.github.com>
2021-10-21 14:31:48 +0100

    inital commit

--- src/friends.py
+++ src/friends.py
@@ -1,2 +1,18 @@
 def get_name(dictionary_name):
     return dictionary_name["name"]
+
+def get_favourite_tv_show(dictionary_name):
+    return dictionary_name["favourites"]["tv_show"]
+
+def likes_to_eat(dictionary_name, food_type):
+    likes = False
+    for snack in dictionary_name["favourites"]["snacks"]:
+        if snack == food_type:
+         likes = True
+    return likes
+
+def add_friend(dictionary_name, friend_name):
+    dictionary_name["friends"].append(friend_name)
+
+
+
